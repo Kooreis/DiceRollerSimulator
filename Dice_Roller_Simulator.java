52d4c9ff317fbc71613c0b6467716904791b9d4d
Kooreis <kooreis.official@gmail.com>
2022-03-09 17:00:00 -0600

    Imported the necessary libraries for the program. Random for generating random numbers and Scanner for user input.

--- Dice_Roller_Simulator.java
+++ Dice_Roller_Simulator.java
@@ -1,22 +1,2 @@
-```java
 import java.util.Random;
 import java.util.Scanner;
-
-public class DiceRollerSimulator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        String userInput = "";
-
-        do {
-            System.out.println("Rolling the dice...");
-            System.out.println("You rolled a " + (random.nextInt(6) + 1));
-            System.out.println("Would you like to roll again? (yes/no)");
-            userInput = scanner.nextLine();
-        } while(userInput.equalsIgnoreCase("yes"));
-
-        System.out.println("Thank you for using Dice Roller Simulator. Goodbye!");
-        scanner.close();
-    }
-}
-```
