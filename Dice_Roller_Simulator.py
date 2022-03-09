f85fb66748a0e5b72842c891913565b2f21e9e23
Kooreis <kooreis.official@gmail.com>
2022-03-09 17:00:00 -0600

    Importing the random module to generate random numbers.

--- Dice_Roller_Simulator.py
+++ Dice_Roller_Simulator.py
@@ -1,11 +1,1 @@
 import random
-
-while True:
-    print("Rolling the dice...")
-    print("The values are:")
-    print(random.randint(1, 6))
-    print(random.randint(1, 6))
-
-    roll_again = input("Roll the dice again? (Y/N) ")
-    if roll_again.lower() != "y":
-        break
