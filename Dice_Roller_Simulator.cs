0faa988e0d61feed4356ff9b495f6310c716263c
Kooreis <kooreis.official@gmail.com>
2022-03-09 17:00:00 -0600

    Initial setup of the DiceRollerSimulator program. A Random object and a string for user input are declared. The program then outputs a welcome message.

--- Dice_Roller_Simulator.cs
+++ Dice_Roller_Simulator.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 
 namespace DiceRollerSimulator
@@ -11,33 +10,6 @@ namespace DiceRollerSimulator
             string userInput = "";
 
             Console.WriteLine("Welcome to Dice Roller Simulator!");
-
-            while (true)
-            {
-                Console.WriteLine("Please enter the number of sides for the dice:");
-                userInput = Console.ReadLine();
-
-                if (int.TryParse(userInput, out int numberOfSides))
-                {
-                    int diceRoll = random.Next(1, numberOfSides + 1);
-                    Console.WriteLine($"You rolled a {diceRoll}!");
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input. Please enter a valid number.");
-                }
-
-                Console.WriteLine("Would you like to roll again? (yes/no)");
-                userInput = Console.ReadLine().ToLower();
-
-                if (userInput != "yes")
-                {
-                    break;
-                }
-            }
-
-            Console.WriteLine("Thank you for using Dice Roller Simulator. Goodbye!");
         }
     }
 }
-```
