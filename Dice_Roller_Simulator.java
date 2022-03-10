76819a5995f6b6c6fded71ff437ca4eb00cbd8b3
Kooreis <kooreis.official@gmail.com>
2022-03-10 17:00:00 -0600

    Created the main class DiceRollerSimulator and the main method. Initialized the Scanner, Random objects and a String to hold user input.

--- Dice_Roller_Simulator.java
+++ Dice_Roller_Simulator.java
@@ -1,2 +1,5 @@
-import java.util.Random;
-import java.util.Scanner;
+public class DiceRollerSimulator {
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+Random random = new Random();
+String userInput = "";
